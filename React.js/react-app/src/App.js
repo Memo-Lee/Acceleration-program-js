dd40820514e732294635f8a6e0763c58c4c9ef05
MemoLee <softwarewewo@gmail.com>
2023-01-09 12:43:41 +0300

    key prop

--- React.js/react-app/src/App.js
+++ React.js/react-app/src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
 import User from "./components/User";
+
+const friends = [
+  {
+    id:0,
+    name:"Ahmet",
+  },
+  {
+    id:1,
+    name:"Erdem",
+  },
+  {
+    id:2,
+    name:"Beyza",
+  },
+];
 function App() {
   return (
     <>
-      <User name="Mehmet" surname="Seven" age={24} isLoggedIn={true} />
+      <User 
+        name="Mehmet" 
+        surname="Seven" 
+        age={24} 
+        isLoggedIn={true} 
+        friends={friends}
+        />
     </>
   );
 }
--- React.js/react-app/src/components/User.js
+++ React.js/react-app/src/components/User.js
@@ -1,5 +1,16 @@
 // props
-function User({name,surname,age,isLoggedIn}) {
-    return <h1> {isLoggedIn ? `${name} ${surname} (${age})`: "Giriş yapmadınız."}</h1>
-        }
+function User({name,surname,age,isLoggedIn,friends}) {
+    return (
+        <>
+            <h1> 
+                {isLoggedIn ? `${name} ${surname} (${age})`: "Giriş yapmadınız."}
+            </h1>
+            {
+                friends.map((friends) => (
+                    <div key={friends.id}>{friends.name}</div>
+                ))
+            }
+        </>
+        ); 
+    }
 export default User;
