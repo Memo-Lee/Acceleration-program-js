603acf1613244ea041272880e394efd20d2db07d
MemoLee <softwarewewo@gmail.com>
2023-01-09 12:31:45 +0300

    props

--- React.js/react-app/src/App.js
+++ React.js/react-app/src/App.js
@@ -1,16 +1,9 @@
 import './App.css';
-import Header from "./components/Header";
-const name = "Mehmet";
-const surname = "Sonmez";
-const isLoggedIn = true; // koşullu render
+import User from "./components/User";
 function App() {
   return (
     <>
-      <Header />
-      <h1>
-        {isLoggedIn ? `Benim adım ${name} Soyadım ${surname}`: "Giriş Yapmadınız"};
-      </h1>
-      {/* {!isLoggedIn && "Giriş Yapmadınız."} */}
+      <User name="Mehmet" surname="Seven" age={24} isLoggedIn={true} />
     </>
   );
 }
---
+++ React.js/react-app/src/components/User.js
@@ -0,0 +1,5 @@
+// props
+function User({name,surname,age,isLoggedIn}) {
+    return <h1> {isLoggedIn ? `${name} ${surname} (${age})`: "Giriş yapmadınız."}</h1>
+        }
+export default User;
