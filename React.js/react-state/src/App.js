02dbf35f0e84a3f42c7f2193846b21a814dc7d12
MemoLee <softwarewewo@gmail.com>
2023-01-10 11:08:49 +0300

    arraystates

--- React.js/react-state/src/App.js
+++ React.js/react-state/src/App.js
@@ -5,13 +5,29 @@ function App() {
   // useState => return içerisin de her hangi bir yerde kullanabiliriz.
   const [name,setName] = useState('Mehmet');
   const [age,setAge] = useState(19);
+  const [friends,setFriends] = useState(["Ahmet", "Murat"]);
   return (
     <div className="App">
       <h1>Merhaba {name}</h1>
       <h2>{age}</h2>
       <button onClick={()=> setAge(25)}>Change Age</button>
       <button onClick={()=> setName("Ahmet")}>Change Name</button>
+    <hr/>
+    <br/>
+    <h2>Friends</h2>
+    {friends.map((friends,index)=>(
+      <div key={index}>{friends}</div>
+    ))}
+    <br/>
+    <button onClick={()=> setFriends([...friends,"Ayşe"])}>
+        Add new friends
+    </button>
+    
+    
+    
+    
     </div>
+
   );
 }
 
