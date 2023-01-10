d37379ed0815f8bf8d0c8f8ffa36d7d0866f6dd5
MemoLee <softwarewewo@gmail.com>
2023-01-10 13:22:32 +0300

    lifecycle

--- React.js/react-lifecycle/src/App.js
+++ React.js/react-lifecycle/src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import {useState,useEffect} from 'react';
 
 function App() {
+  /* useEffect ve useState gibi hooklar 
+  if,while vs. gibi kontrol mekanizmalarının içerisinde bulunmaz.*/
+  const [number,setNumber] = useState(0);
+  const [name,setName] = useState("Mehmet");
+  useEffect(() => {
+    console.log("Component mount edildi.");
+  }, []); //dependency array
+  useEffect(()=> {
+    console.log("number state güncellendi");
+  },[number]);
+  useEffect(()=> {
+    console.log("name state güncellendi");
+  },[name]);
   return (
     <div className="App">
-      
+        <h1>{number}</h1>
+        <br/>
+        <button onClick={()=> setNumber(number + 1)}>Click Number</button>
+        <br/>
+        <h1>{name}</h1>
+        <br/>
+        <button onClick={()=> setName("Ahmet")}>Click Name</button>
     </div>
   );
 }
