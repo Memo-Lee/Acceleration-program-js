64686f731d929c9a10234990248d1519dad61cb5
MemoLee <softwarewewo@gmail.com>
2023-01-27 17:29:26 +0300

    lesson1

---
+++ React.js/react-routing/src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
+import Users from './components/Users';
+
+function App() {
+  return (
+    <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/users" element={<Users/>}/>
+      </Routes>
+    </div>
+  </Router>
+  );
+}
+
+export default App;
